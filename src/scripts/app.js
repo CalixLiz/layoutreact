a410abeac5263a9ca7c9d11b1f42befacd754dbb
Lizbeth <Lizbeth@Lizbeths-MacBook-Pro.local>
2017-03-06 19:20:54 -0600

    code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,86 +4,60 @@ import Backbone from 'backbone'
 import init from './init'
 
 
-var ClonePage = React.createClass({
+
+var Resptimeline = React.createClass({
 	render: function() {
-		return (
-			<div className="clonePage">
-				<Wrapper />
-				<ClassContent />
-				<ClassMiddle />
-				<Banner />
+		return <div className="layoutpage">
+		<Timelin />
+		<Picicon />
+		<Textlan />
+		</div>
+	}
+})
+
+var Timelin = React.createClass({
+	render: function() {
+		return 
+			<div className="time">
+				<p>4/10/13</p>
+				<h4>18:30</h4>
 			</div>
-			)
 	}
 })
 
-
-var Wrapper = React.createClass({
+var Picicon = React.createClass({
 	render: function() {
-		return (
-			<div id="wrapper">
-				<img className="skyline" src="http://magentanova.github.io/html-intro-1/images/houston.jpg" />
-        		<img className="logo" src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png" />
-    		</div>
-			)
-	}
-})
-
-var ClassContent = React.createClass({
-	render: function() {
-		return (
-			<div className="left">
-				<p className="left-title"><b>THE IRON YARD | HOUSTON</b></p>
-				<p>Happenings and updates from The Iron Yard in Houston, TX</p>
-				<p>SEARCH</p>
-				<input className="search" placeholder="Search Keywords" />
-				</div>
-			)
-	}
-})
-
-var ClassMiddle = React.createClass({
-	render: function() {
-		return (
-			<div className="middle">
-				<h2>September 22 Starts a New Class of The Iron Yard Houston Students</h2>
-				<p className='img-text'><em><small>By Brian Dorton, Campus Director at <a href='https://www.theironyard.com' target= '_blank'>The Iron Yard</a> Houston</small></em></p>
-            <img className='classroom' src="http://magentanova.github.io/html-intro-1/images/classroom.jpg" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate porro, placeat dolorem maiores distinctio nobis ducimus assumenda hic nam nemo iure illum earum est cum! Rerum eveniet voluptatum facere quaerat.</p>
+		return
+			<div className="icon">
+				<i class="fafamobile" aria-hidden="true"></i>
 			</div>
-			)
+
 	}
 })
 
-var Banner = React.createClass({
+
+var Textlan = React.createClass({
 	render: function() {
-		return(
-			<div id='banner'>
-            <div id='banner-top'>
-                <p><b>Never miss a Post!</b></p>
-            </div>
-            <div id='banner-bottom'>
-                <div id='bottom-left'>
-                    <img src='http://magentanova.github.io/html-intro-1/images/ironyardlogo.png' />
-                </div>
-                <div id='bottom-middle'>
-                    <p id='top-sentence'><b>tiyhouston</b></p>
-                    <p>The Iron Yard | Houston</p>
-                    <hr />
-                </div>
-                <div id='bottom-right'>
-                    <p><b>+ Follow</b></p>
-                </div>
-            </div>
-        </div>
-			)
+		return
+			<div className="textlan">
+				<h5>Ricebean black-eyed pea</h5>
+				<p>Heroku makes it easy to deploy and scale Java apps in the cloud
+				Heroku makes it easy to deploy and scale Java apps in the cloud
+				Heroku makes it easy to deploy and scale Java apps in the cloud
+				</p>
+			</div>
 	}
 })
+
 
 var app = function() {
 	var libraryName = 'react'
-	ReactDOM.render(<ClonePage />, document.querySelector('.container'))
+
+	React.render(<Resptimeline />, document.querySelector('.container'))
+	
 }
+
+
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
 // NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
