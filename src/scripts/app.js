0ececcbb5e96268f885e423669b980a220925c74
Lizbeth <Lizbeth@Lizbeths-MacBook-Pro.local>
2017-03-06 18:40:15 -0600

    more code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,19 +4,52 @@ import Backbone from 'backbone'
 import init from './init'
 
 
-var BurgerPage = React.createClass({
-	// every react component has a method called `.render()` that describes (via its return value) 
-	// what elements that component will contain. in other words, it returns the tree structure, 
-	// written in jsx, that the element will contain.
+var ClonePage = React.createClass({
 	render: function() {
-		return ( // important! you can only return *one element* here. that element can have children,
-			// but it can't have siblings.
-			<div className="burger-page"> {/*if i write a lowercase jsx element, React
-			will treat it as an html tag. */}
-				<Banner /> {/*if i write an uppercase jsx element, React
-			will assume that it is a custom component and look for its definition
-		(Banner is defined below). */}
-				<PageBody />
+		return (
+			<div className="clonePage">
+				<Wrapper />
+				<ClassContent />
+				<ClassMiddle />
+				<Banner />
+			</div>
+			)
+	}
+})
+
+
+var Wrapper = React.createClass({
+	render: function() {
+		return (
+			<div id="wrapper">
+				<img className="skyline" src="http://magentanova.github.io/html-intro-1/images/houston.jpg" />
+        		<img className="logo" src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png" />
+    		</div>
+			)
+	}
+})
+
+var ClassContent = React.createClass({
+	render: function() {
+		return (
+			<div className="left">
+				<p className="left-title"><b>THE IRON YARD | HOUSTON</b></p>
+				<p>Happenings and updates from The Iron Yard in Houston, TX</p>
+				<p>SEARCH</p>
+				<input className="search" placeholder="Search Keywords" />
+				</div>
+			)
+	}
+})
+
+var ClassMiddle = React.createClass({
+	render: function() {
+		return (
+			<div className="middle">
+				<h2>September 22 Starts a New Class of The Iron Yard Houston Students</h2>
+				<p className='img-text'><em><small>By Brian Dorton, Campus Director at <a href='https://www.theironyard.com' target= '_blank'>The Iron Yard</a> Houston</small></em></p>
+            <img className='classroom' src="http://magentanova.github.io/html-intro-1/images/classroom.jpg" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate porro, placeat dolorem maiores distinctio nobis ducimus assumenda hic nam nemo iure illum earum est cum! Rerum eveniet voluptatum facere quaerat.</p>
 			</div>
 			)
 	}
@@ -24,53 +57,36 @@ var BurgerPage = React.createClass({
 
 var Banner = React.createClass({
 	render: function() {
-		return (
-			<div className="banner">
-				<h1>THE BURGER STORE</h1>
-				<NavBar />
-			</div>
-			)
-	} 
-})
-
-var NavBar = React.createClass({
-	render: function() {
-		return (
-			<div className="nav-bar">
-				<a href="#home">home</a>
-				<a href="#location">location</a>
-				<a href="#menu">menu</a>
-			</div>
-			)
-	}
-})
-
-var PageBody = React.createClass({
-	render: function() {
-		return (
-			<div className="page-body">
-				<img src="https://i.ytimg.com/vi/a40xP-CvBAw/maxresdefault.jpg"/>
-				<p>Burgers are better with cheese</p>
-			</div>
+		return(
+			<div id='banner'>
+            <div id='banner-top'>
+                <p><b>Never miss a Post!</b></p>
+            </div>
+            <div id='banner-bottom'>
+                <div id='bottom-left'>
+                    <img src='http://magentanova.github.io/html-intro-1/images/ironyardlogo.png' />
+                </div>
+                <div id='bottom-middle'>
+                    <p id='top-sentence'><b>tiyhouston</b></p>
+                    <p>The Iron Yard | Houston</p>
+                    <hr />
+                </div>
+                <div id='bottom-right'>
+                    <p><b>+ Follow</b></p>
+                </div>
+            </div>
+        </div>
 			)
 	}
 })
 
 var app = function() {
-  var libraryName = 'react'
-  // ReactDOM.render() takes two inputs: 
-  	// 1. A React component
-  	// 2. A node on the actual DOM — the place we want to mount our React node tree.
-  ReactDOM.render(<BurgerPage />, document.querySelector('.container'))
-  // this is where i MOUNT my components. to mount is to sync up your virtual DOM 
-  // tree with the actual DOM. a React component is mounted if it's curently part 
-  // of the web document. it is unmounted if it's just floating around in javscriptland.
-
-
+	var libraryName = 'react'
+	ReactDOM.render(<ClonePage />, document.querySelector('.container'))
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
 // NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
-export var app_name = init()
+export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
